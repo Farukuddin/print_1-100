480d690e23ba4fad8d8ea160da102a8f3d4573ac
Farukuddin <106924931+Farukuddin@users.noreply.github.com>
2023-02-14 12:39:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# print_1-100
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ahbhcx)
---
+++ index.js
@@ -0,0 +1,12 @@
+// run `node index.js` in the terminal
+
+let i;
+for (i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    document.write('amazon' + ' ');
+  } else if (i % 3 == 0) document.write('google' + ' ');
+  else if (i % 5 == 0) document.write('facebook' + ' ');
+  // print the number
+  else document.write(i + ' ');
+}
+
